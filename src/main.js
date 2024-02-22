9befea6acfb62d84593f622e20196ec435923f2b
Alex <112724167+klimbass@users.noreply.github.com>
2024-02-22 16:38:20 +0200

    add_hw

---
+++ src/main.js
@@ -0,0 +1,70 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import fetchPixabayAPI from './js/pixabay-api.js';
+import renderGallery from './js/render-functions.js';
+
+import icon from './img/icon-close.svg';
+
+const options = new URLSearchParams({
+  key: '4348225-c90d49c3185e1d6dd0df9aa80',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 21,
+});
+
+const loader = '<span class="loader"></span>';
+
+const gallery = document.querySelector('.gallery');
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchRequest = event.target.elements.input.value.trim();
+  if (searchRequest) {
+    gallery.innerHTML = loader;
+    options.set('q', searchRequest);
+    fetchPixabayAPI(options)
+      .then(data => {
+        if (data.hits.length === 0) {
+          gallery.innerHTML = '';
+          iziToast.error({
+            position: 'topRight',
+            maxWidth: '380px',
+            progressBar: false,
+            iconUrl: icon,
+            transitionIn: 'fadeInLeft',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+        } else {
+          gallery.innerHTML = renderGallery(data.hits);
+          const gallerySimple = new SimpleLightbox('.gallery a', {
+            captionsData: 'alt',
+            captionDelay: 250,
+          });
+          gallerySimple.refresh();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    console.log('Put the request!');
+    iziToast.show({
+      progressBar: false,
+      position: 'topRight',
+      maxWidth: '380px',
+      iconUrl: icon,
+      transitionIn: 'fadeInLeft',
+      message: 'Put the request!',
+    });
+  }
+  form.reset();
+});
